a7dc50f6d03278f2f73a280f3ef92a6af9e5d683
Wallace Rio <wallrio@gmail.com>
2017-10-20 12:29:53 -0200

    ajuste em logo e cores

--- wp-config.php
+++ wp-config.php
@@ -27,7 +27,7 @@ define('DB_NAME', 'viajantespiratasblog');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'wallace4123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -78,7 +78,8 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+// define( 'WP_DEBUG_DISPLAY', true ); 
+// define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy blogging. */
 
